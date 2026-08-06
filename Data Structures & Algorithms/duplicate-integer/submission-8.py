b756546e19cc68f29b1a82400bcaa857bc166515
tylertamalunas <46872798+tylertamalunas@users.noreply.github.com>
2026-08-06 10:26:42 -0500

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # hash set
+        new_set = set(nums)
+
+        if len(new_set) != len(nums):
+            return True
+        else:
+            return False
